0c799a37f905b164a6e078a54e8fa0fc9eae7ec1
Evgeny Tropkin <evgeny-tropkin@yandex.ru>
2022-03-26 10:23:22 +0300

    To-Do List. Stage 2/4. script.js: "addNewTask" procedure implemented

---
+++ To-Do List/task/src/script.js
@@ -0,0 +1,25 @@
+function addNewTask () {
+    let taskInputField = document.getElementById("input-task");
+    let taskText = taskInputField.value;
+    if (taskText !== ''){
+        let tasks = document.getElementById("task-list")
+        let newTask = document.createElement("li");
+        let taskCheck = document.createElement("input");
+        let taskDescription = document.createElement("span");
+        let taskDelete = document.createElement("button");
+
+        newTask.className = "task-row";
+        taskCheck.type = "checkbox";
+        taskDescription.className = "task";
+        taskDescription.innerHTML= taskText;
+        taskDelete.className = "delete-btn";
+        taskDelete.innerHTML = "Delete";
+
+        newTask.appendChild(taskCheck);
+        newTask.appendChild(taskDescription);
+        newTask.appendChild(taskDelete);
+        tasks.appendChild(newTask);
+    }
+}
+
+document.getElementById("add-task-button").addEventListener("click", addNewTask);
